f239594162a9dc09864fbc4949c92c577b97cff5
yaozy <freeoasoft@126.com>
2017-10-14 19:49:28 +0800

    init

---
+++ performance.js
@@ -0,0 +1,38 @@
+let RedisClient = require('./lib');
+
+(async function () {
+
+    let client = new RedisClient(/*{ host: '127.0.0.1', port: 6379, pool: 2 }*/);
+    
+    await client.ready();
+    
+    let date = new Date();
+
+    for (var i = 0; i < 1000; i++)
+    {
+        let value;
+
+        value = await client.hset('test', 'a', '123\r\n456');
+        value = await client.hget('test', 'a');
+
+        value = await client.hset('test', 'b', '1234567891011');
+        value = await client.hget('test', 'b');
+
+        value = await client.hset('test', 'c', '123\r\n456');
+        value = await client.hget('test', 'c');
+
+        value = await client.hset('test', 'd', '1234567891011');
+        value = await client.hget('test', 'd');
+
+        value = await client.hset('test', 'e', '123\r\n456');
+        value = await client.hget('test', 'e');
+
+        value = await client.hmset('test', { f: 1, g: 2 });
+        value = await client.hmget('test', 'f', 'g');
+
+        value = await client.hgetall('test');
+    }
+
+    console.log(new Date() - date);
+
+})();
---
+++ readme.md
@@ -0,0 +1,31 @@
+# redis-await
+light weight and high performance async nodejs redis client! support await
+
+## Installation
+`npm install redis-await`
+
+## Usage
+    const RedisClient = require('redis-await');
+
+    (async function () {
+
+        let client = new RedisClient(/*{ host: '127.0.0.1', port: 6379, pool: 2 }*/);
+        let value;
+        
+        await client.ready();
+        
+        value = await client.hset('test', 'key', '123\r\n456');
+        value = await client.hget('test', 'key');
+
+        value = await client.hmset('test', { key1: 1, key2: 2 });
+        value = await client.hmget('test', 'key1', 'key2');
+
+        value = await client.hgetall('test');
+
+        //use command
+        value = await client.command('HGETALL', 'test');
+
+        //direct send redis instruction
+        value = await client.send('*2\r\n$7\r\nHGETALL\r\n$4\r\ntest\r\n');
+
+    })();
